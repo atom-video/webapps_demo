442c0e5b2ff2baa031c66d30b7990c7bb5c35c40
SerKKiT <serjtimakov@yandex.ru>
2023-06-06 20:37:23 +0300

    Working page!!!!

--- videojs-hls-streams.js
+++ videojs-hls-streams.js
@@ -1,40 +1,67 @@
-function loadHlsStreams() {
-  fetch('https://filespot.platformcraft.ru/2/fs/container/60b080470e47cf6763e5ae85/object/kit', {
-    headers: {
-      'Authorization': 'Bearer eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1IiwibmFtZSI6ImdyZWVuYXRvbSIsImdyb3VwIjoiIiwib3duZXJfbmFtZSI6ImdyZWVuYXRvbSIsInBlcm1pc3Npb25zIjp7IkNETl9zdGF0IjpbeyJpZCI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsIm93bmVyIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1IiwicmlnaHRzIjo3LCJhZHZhbmNlZCI6bnVsbH1dLCJGaWxlc3BvdCI6W3siaWQiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJvd25lciI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsInJpZ2h0cyI6NywiYWR2YW5jZWQiOm51bGx9XSwiSWRlbnRpdHkgU2VydmljZSI6W3siaWQiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJvd25lciI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsInJpZ2h0cyI6NywiYWR2YW5jZWQiOm51bGx9XSwiUmVjb3JkZXIiOlt7ImlkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1Iiwib3duZXIiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJyaWdodHMiOjcsImFkdmFuY2VkIjp7InNpbXVsdF9saW1pdCI6M319XSwiU3RyZWFtUHVibGlzaCI6W3siaWQiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJvd25lciI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsInJpZ2h0cyI6NywiYWR2YW5jZWQiOm51bGx9XSwiU3RyZWFtZXIiOlt7ImlkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1Iiwib3duZXIiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJyaWdodHMiOjcsImFkdmFuY2VkIjpudWxsfV0sIlRyYW5zY29kZXIiOlt7ImlkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1Iiwib3duZXIiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJyaWdodHMiOjcsImFkdmFuY2VkIjpudWxsfV19LCJzdXBlciI6ZmFsc2UsImV4cCI6MTY4NjA0ODM2NSwiaWF0IjoxNjg1OTYxOTY1LCJpc3MiOiJhdXRoLnBsYXRmb3JtY3JhZnQucnUifQ.d9Tw874r-iDWVpk05zZxGRAj1Ug6yXm3YIKO7A2nGWIhl33rO-B1zOlltTzwtsfOcXvsQh0oESjm_uVViLfUsmLy0siW3BEknyFLG-hsshy9pQG4-tO0-Hp6hsPYrVTOEJdpHmC9ADO55dq2W1s4zcqX0dS0bqz_w9wclg1FrgF_bSUn2cmXZ5cyIPlcavuZzXVHEUWvcR60WUkX-dLr_-dH7v_84iLFA3NVA38tlI4XFYAYmQdfCGpi15T4pMHMY9xjuKcSilTSgO2KvEmN4WvP7tcEW7pk0uUX_vE8ecjTza0EZS-a10sBM7Y0WiJLuG6NyclPZ9w6mX-aZDY0FQ'
-    }
-  })
+const headers = new Headers();
+headers.append('Authorization', 'Bearer eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1IiwibmFtZSI6ImdyZWVuYXRvbSIsImdyb3VwIjoiIiwib3duZXJfbmFtZSI6ImdyZWVuYXRvbSIsInBlcm1pc3Npb25zIjp7IkNETl9zdGF0IjpbeyJpZCI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsIm93bmVyIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1IiwicmlnaHRzIjo3LCJhZHZhbmNlZCI6bnVsbH1dLCJGaWxlc3BvdCI6W3siaWQiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJvd25lciI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsInJpZ2h0cyI6NywiYWR2YW5jZWQiOm51bGx9XSwiSWRlbnRpdHkgU2VydmljZSI6W3siaWQiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJvd25lciI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsInJpZ2h0cyI6NywiYWR2YW5jZWQiOm51bGx9XSwiUmVjb3JkZXIiOlt7ImlkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1Iiwib3duZXIiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJyaWdodHMiOjcsImFkdmFuY2VkIjp7InNpbXVsdF9saW1pdCI6M319XSwiU3RyZWFtUHVibGlzaCI6W3siaWQiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJvd25lciI6IjYwYjA4MDQ3MGU0N2NmNjc2M2U1YWU4NSIsInJpZ2h0cyI6NywiYWR2YW5jZWQiOm51bGx9XSwiU3RyZWFtZXIiOlt7ImlkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1Iiwib3duZXIiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJyaWdodHMiOjcsImFkdmFuY2VkIjpudWxsfV0sIlRyYW5zY29kZXIiOlt7ImlkIjoiNjBiMDgwNDcwZTQ3Y2Y2NzYzZTVhZTg1Iiwib3duZXIiOiI2MGIwODA0NzBlNDdjZjY3NjNlNWFlODUiLCJyaWdodHMiOjcsImFkdmFuY2VkIjpudWxsfV19LCJzdXBlciI6ZmFsc2UsImV4cCI6MTY4NjE1MjQxOSwiaWF0IjoxNjg2MDY2MDE5LCJpc3MiOiJhdXRoLnBsYXRmb3JtY3JhZnQucnUifQ.T2F-s_-mQniJaPF0xbEdmpLzkMzeveBKLPn5OrBS39VJdFEm4gMrbkDYYTwnSdGGWWpsT2Ej9EKIs-6jcCXEvdHt-ywRL5Qkz5v1Zep9Sys30snOiT7feOc6T6dcpgWehyfYK1Ph39rL5VjAOcKgi1kuzRbUYaA5OvyA5wGW-VUnGYUoX9U1FZCnBhacHol3H7f3IA33_53b6eQxCHra-P-oIeJ6m9BN0B9KFGZ7YS1e_C7B7RU-vj7DdsXwRxswNWkfN9dRip7nlw0-Cs2qod9Z6ZwS9uIRO9iRDXm-Xbm4ZWKjgA-zMyIY5SLkMq-UFMUBMX_XN4yloPGx27kGlA');
+
+const init = {
+  method: 'GET',
+  headers
+};
+
+let currentLinks = []; // keep track of current links
+let videoElements = []; // keep track of video elements
+let videoPlayers = []; // keep track of video players
+
+function updateStreams() {
+  fetch('https://filespot.platformcraft.ru/2/fs/container/60b080470e47cf6763e5ae85/object/kit', init)
     .then(response => response.json())
     .then(data => {
-      const linkList = data.contents.map(link => ('https://'+link.download_url));
-      console.log(linkList);
-      
-      // Display the video players
-      linkList.forEach((link, index) => {
-        const playerId = `player${index + 1}`;
-        const videoContainer = document.createElement('div');
-        videoContainer.id = playerId;
-        videoContainer.classList.add('video-container');
-        document.getElementById('players-container').appendChild(videoContainer);
-        
-        const playerOptions = {
-          controls: true,
-          autoplay: false,
-          fluid: true,
-          sources: [
-            {
-              src: link,
-              type: 'application/x-mpegURL'
-            }
-          ]
-        };
-        
-        videojs(playerId, playerOptions);
-      });
-    })
-    .catch(error => {
-      console.error('Error:', error);
+      const newLinks = data.contents.map(link => ('https://' + link.download_url));
+      const changedLinks = newLinks.filter((link, index) => link !== currentLinks[index]);
+      if (changedLinks.length > 0) { // only update if links have changed
+        currentLinks = newLinks;
+        const videoContainer = document.getElementById('video-container');
+        const existingVideoElements = Array.from(videoContainer.getElementsByTagName('video'));
+        const existingLinks = existingVideoElements.map(videoElement => videoElement.getElementsByTagName('source')[0].getAttribute('src'));
+        const existingLinksSet = new Set(existingLinks);
+        changedLinks.forEach(link => {
+          if (existingLinksSet.has(link)) {
+            existingLinksSet.delete(link);
+          } else {
+            const videoElement = document.createElement('video');
+            videoElement.setAttribute('class', 'video-js vjs-default-skin vjs-big-play-centered');
+            videoElement.setAttribute('controls', '');
+            videoElement.setAttribute('preload', 'auto');
+            videoElement.setAttribute('width', '640');
+            videoElement.setAttribute('height', '480');
+            const sourceElement = document.createElement('source');
+            sourceElement.setAttribute('src', link);
+            sourceElement.setAttribute('type', 'application/x-mpegURL');
+            videoElement.appendChild(sourceElement);
+            videoElements.push(videoElement); // add new element to array
+            videoContainer.appendChild(videoElement);
+            const player = videojs(videoElement);
+            videoPlayers.push(player); // add new player to array
+          }
+        });
+        videoPlayers.forEach(player => {
+          const link = player.currentSrc();
+          if (!changedLinks.includes(link)) {
+            const index = videoPlayers.indexOf(player);
+            videoPlayers.splice(index, 1); // remove player from array
+            player.dispose(); // destroy player
+          }
+        });
+        videoElements.forEach(videoElement => {
+          const link = videoElement.getElementsByTagName('source')[0].getAttribute('src');
+          if (!changedLinks.includes(link)) {
+            const index = videoElements.indexOf(videoElement);
+            videoElements.splice(index, 1); // remove element from array
+            videoElement.parentNode.removeChild(videoElement); // remove element from page
+          }
+        });
+      }
     });
 }
 
-window.addEventListener('DOMContentLoaded', loadHlsStreams);
+updateStreams(); // fetch streams once on page load
+
+setInterval(updateStreams, 10000); // update streams every 10 seconds
